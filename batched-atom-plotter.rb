21f24ebaf263aba68147ded30c43771ce05a10ef
TaufeqRazakh <taufeqmdr@gmail.com>
2020-09-20 22:44:03 -0700

    added read functionality for .xyz files. tested for forst line and ground truth positions

--- batched-atom-plotter.rb
+++ batched-atom-plotter.rb
@@ -4,12 +4,17 @@
 # ruby batched-atom-plotter.rb "result_set_*_of_five.txt"
 
 require 'humanize'
+require 'rainbow/refinement'
+
+using Rainbow
 
 @sorted_files = 5.times.map {|x| "result_set_"+(x+1).to_i.humanize+"_of_five.txt"}
 
 @steps_per_batch = [4, 5, 8, 6, 6]
 
-@x = 3.7731
+@sorted_files_and_steps = @sorted_files.zip(@steps_per_batch)
+
+@x = 0
 @y = 1.5290
 @z = 3.6253
 
@@ -33,7 +38,7 @@ def get_offset_from_line_start(file_descriptor)
   offset
 end
 
-def get_co_ordinate(file_descriptor, offset, steps)
+def get_traj_co_ordinates_in_dump(file_descriptor, offset, steps)
   co_ords = []
   steps.times {
     file_descriptor.seek(offset, IO::SEEK_CUR)
@@ -44,28 +49,58 @@ def get_co_ordinate(file_descriptor, offset, steps)
   }
   co_ords
 end
+
 # open the dump files and read the x,y and z positions for those 
 # many steps
-def open_file_and_locate_co_ordinates(file_name, steps)
+def open_dump_and_locate_co_ordinate(file_name, steps)
   fd = File.new(file_name)
 
   found_locations = locate_co_ordinates(file_name)
-  p found_locations
+  puts "found locations for "+@x.inspect.green+" in "+file_name.blue+" to be #{found_locations}"
   
   fd.seek(found_locations.first - 1)
   offset = get_offset_from_line_start(fd)
-  p offset
+  puts "offset distance from line start to arrive at co-ordinate is #{offset}"
   
-  @x_s = get_co_ordinate(fd, offset, steps)
-  p @x_s
+  co_ordinates_in_dump = get_traj_co_ordinates_in_dump(fd, offset, steps)
+  @x_s = @x_s.union(co_ordinates_in_dump)
+  @x = @x_s.last
+  puts "co-ordinates obtained for atom in this dimension so far are \u{1F373} :\n#{@x_s}"
   
 end
 
+def read_start_points_from_xyz(file_name)
+  fd = File.new(file_name)
+  
+  # .xyz file has first line with number of atoms
+  number_of_atoms = fd.readline.to_i
+  
+  # .xyz has second line empty
+  fd.readline
+  
+  # .read starting co-ordinated from lines
+  line = fd.readline
+  
+  # divide line into data
+  line_data = line.split
+  output_file_name = "atom_traj_" + line_data.shift
+  
+  # read the starting co-ordinate for each dimension
+  while (starting_co_ordinate = line_data.shift) do 
+    @x = starting_co_ordinate.to_f.round(4)
+    puts "\u{1F600} initiaing trajectory grab starting from #{@x}"
+    @x_s = []
+    @sorted_files_and_steps.each do |file_name, steps|
+      open_dump_and_locate_co_ordinate(file_name, steps)
+    end
+  end
+  
+end
 
 # Print the tracked atom positions into an output file for wolfram
 
 # Perform open_file_and_populate for eatch batch
 
 # Start 
-open_file_and_locate_co_ordinates(@sorted_files.first, @steps_per_batch.first)  
-
+# open_dump_and_locate_co_ordinates(@sorted_files.first, @steps_per_batch.first)
+read_start_points_from_xyz('frame130.xyz')
